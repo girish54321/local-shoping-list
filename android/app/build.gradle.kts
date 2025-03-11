59a252508fe9afe2124e23065ccc455d36506954
Girish Parate <girishparate@Girishs-MacBook-Pro.local>
2025-03-11 06:13:08 +0530

    clean up the code

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -31,7 +31,12 @@ android {
     }
 
     signingConfigs {
-        
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+            storePassword = keystoreProperties["storePassword"] as String
+        }
     }
 
     defaultConfig {
@@ -46,7 +51,11 @@ android {
     }
 
     buildTypes {
-    
+        release {
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
+           signingConfig = signingConfigs.getByName("release")
+        }
     }
 }
 
